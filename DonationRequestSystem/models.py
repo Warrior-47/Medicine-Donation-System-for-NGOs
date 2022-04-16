bff38f1ce811c55155b8e18ffb2c17a87981c56c
Your NameAshfaq777 <you@example.commohammed.ashfaqul.haque@g.bracu.ac.bd>
2022-04-17 05:40:04 +0600

    donationRequest Table created

---
+++ DonationRequestSystem/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from account.models import CustomUser
+
+# Create your models here.
+class donationRequest(models.Model):
+    Donor_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    NGO_id = models.CharField(max_length=10, blank=True)
+    Medicine_list = models.CharField(max_length=11, blank=True)
+    Delivery_type = models.CharField(max_length=11,blank=True)
+    Pick_Up_address = models.CharField(max_length=150,blank=True)
+    Acceptance_Status = models.BooleanField(default=False)
+    Pick_Up_date = models.DateField()
+    Pick_Up_time = models.TimeField()
+    Delivery_status = models.CharField(max_length=20,default='pending')
+
+
+ 
