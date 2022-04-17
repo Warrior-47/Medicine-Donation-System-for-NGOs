3cc74f4ba3a8cfbf617ad5eb6cbddd9077d9b09c
Your NameAshfaq777 <you@example.commohammed.ashfaqul.haque@g.bracu.ac.bd>
2022-04-17 06:19:03 +0600

    donationRequest table v2

--- DonationRequestSystem/models.py
+++ DonationRequestSystem/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 from account.models import CustomUser
 
@@ -16,4 +15,5 @@ class donationRequest(models.Model):
     Delivery_status = models.CharField(max_length=20,default='pending')
 
 
- 
+    def __str__(self):
+        return f'{self.Donor_id,self.NGO_id}'
