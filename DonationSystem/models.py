777ff817fc4c5940929fbfe39fb7e29f0bf894da
SamiraAfrin <74605273+SamiraAfrin@users.noreply.github.com>
2022-04-21 16:23:31 +0600

    Added Medicine list functionality

--- DonationSystem/models.py
+++ DonationSystem/models.py
@@ -1,3 +1,29 @@
+from email.policy import default
 from django.db import models
+# from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class NGO_MedicineListInfo(models.Model):
+    MedicineName = models.CharField(max_length=100, blank=False)
+    DosageAmount = models.IntegerField(validators=[MinValueValidator(0)],blank=False)
+    MedicinePriority = models.PositiveIntegerField(validators=[MaxValueValidator(3)],blank=False)
+    AmountRequired = models.PositiveIntegerField(validators=[MinValueValidator(1)],blank=False)
+    NGO = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"NGO | {self.MedicineName} Priority: {self.MedicinePriority}"
+
+class Donor_MedicineListInfo(models.Model):
+    MedicineName = models.CharField(max_length=100, blank=False)
+    DosageAmount = models.IntegerField(validators=[MinValueValidator(0)],blank=False)
+    PillsLeft = models.IntegerField(validators=[MinValueValidator(0)],blank=False)
+    ExpiryDateImage = models.ImageField(upload_to='medicine_images')
+
+    Donor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Donor | {self.MedicineName} Dosage: {self.DosageAmount}"
+
--- DonationSystem/views.py
+++ DonationSystem/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from DonationSystem.models import Donor_MedicineListInfo, NGO_MedicineListInfo, Donor_MedicineListInfo
+from account.models import CustomUser
+
 
 # Create your views here.
 def dashboard(request):
+    if request.user.is_authenticated:
+        if request.user.is_ngo:
+            context = NGO_MedicineListInfo.objects.filter(NGO=request.user)
+        else:
+            context = Donor_MedicineListInfo.objects.filter(Donor=request.user)
+        
+        return render(request, 'DonationSystem/dashboard.html',{'context':context, 'is_ngo': request.user.is_ngo })
+
     return render(request, 'DonationSystem/dashboard.html')
