974a2da780afffe04088a8fbe1a80791c5f7bf8d
SamiraAfrin <74605273+SamiraAfrin@users.noreply.github.com>
2022-04-24 23:48:42 +0600

    Added update delete functionality

--- DonationSystem/urls.py
+++ DonationSystem/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('edit-list/', views.edit_list, name='edit-list'),
+    path('add-medicine/', views.add_medicine, name='add-medicine'),
+    path('update-medicine/<int:pk>/', views.update_medicine, name='update-medicine'),
+    path('delete-medicine/<int:pk>/', views.delete_medicine, name='delete-medicine'),
 ]
