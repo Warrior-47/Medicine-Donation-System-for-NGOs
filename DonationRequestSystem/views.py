66139888fa4875c905cb47723ed7d30f0233eac9
Sakib Tapu <59693766+Warrior-47@users.noreply.github.com>
2022-04-25 01:31:57 +0600

    Donors can now see notification

--- DonationRequestSystem/views.py
+++ DonationRequestSystem/views.py
@@ -10,7 +10,7 @@ from  account.models import CustomUser
 from DonationSystem.models import Donor_MedicineListInfo,NGO_MedicineListInfo
 from DonationRequestSystem.models import donationRequest,donatedMedicines
 
-
+# Donation form for donor
 def donation(request,pk):
     if request.method == 'POST':
         form = donationRequestForm(request.POST)
@@ -41,7 +41,7 @@ def donation(request,pk):
         
     return render(request, 'DonationRequestSystem/donation_request.html', {'form': form, 'title': 'donation'})
 
-
+# Notification Page
 def donation_decision(request):
     if request.user.is_authenticated:
         if request.user.is_ngo:
@@ -51,33 +51,47 @@ def donation_decision(request):
                 if len(donatedMedicines.objects.filter(donation_request=i)) != 0:
                     context.append(i)
 
-            return render(request,  'DonationRequestSystem/ngo_notification.html',{'context':context, 'is_ngo': request.user.is_ngo })
+            accepted_donors = donationRequest.objects.filter(NGO=request.user,Acceptance_Status=True)
+            return render(request,  'DonationRequestSystem/ngo_notification.html',{ 'context':context, 'is_ngo': request.user.is_ngo, 'accepted_donors':accepted_donors })
 
+        else:
+            req_data = donationRequest.objects.filter(Donor=request.user)
+            context = list(req_data.filter(Acceptance_Status=True))
+            
+            for i in req_data.filter(Acceptance_Status=False):
+                if len(donatedMedicines.objects.filter(donation_request=i)) == 0:
+                    context.append(i)
+
+            return render(request,  'DonationRequestSystem/ngo_notification.html',{ 'context':context, 'is_ngo': request.user.is_ngo })
+    
     return render(request, 'DonationRequestSystem/ngo_notification.html')
 
-
+# Choose Pickup Date and Time for NGO
 def donationDetails(request,pk):
-    
     request_obj = donationRequest.objects.get(pk=pk)
     form = deliveryDetails(instance=request_obj)
+
     if request.method == 'POST':
         form = deliveryDetails(request.POST)
         if form.is_valid():
             date = form.cleaned_data.get('pickupDate')
             time = form.cleaned_data.get('pickupTime')
             donationRequest.objects.filter(pk=pk).update(Pick_Up_date=date, Pick_Up_time=time,Acceptance_Status=True)
+            
+            return redirect('ngo_notification')
 
     return render(request, 'DonationRequestSystem/donation_details.html',{'form': form, 'title': 'donation'})        
 
-
+# NGO Accepting In-Person Request
 def donationDetailsInPerson(request,pk):
     
     request_obj = donationRequest.objects.get(pk=pk)
     
     donationRequest.objects.filter(pk=pk).update(Acceptance_Status=True)
 
-    return redirect('dashboard')     
+    return redirect('ngo_notification')
 
+# NGO Rejecting Request
 def donationReject(request, pk):
     donatedMedicines.objects.filter(donation_request=pk).delete()
     return redirect('dashboard')
