8c882e3069ac803992f88e592afa3676f4a074c5
Sakib Uddin Tapu <59693766+Warrior-47@users.noreply.github.com>
2024-08-25 23:09:35 +0200

    chore: added versions to modules in pipfile and updated settings.py

--- Medicine_Donation_System_for_NGOs/settings.py
+++ Medicine_Donation_System_for_NGOs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'DonationSystem.apps.DonationsystemConfig',
     'search.apps.SearchConfig',
     'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
