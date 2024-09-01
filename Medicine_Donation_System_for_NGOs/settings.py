d3d2ea3610cdf366063ea226b40ab0170eca65d1
Sakib Uddin Tapu <59693766+Warrior-47@users.noreply.github.com>
2024-09-01 18:48:26 +0200

    fix: added ALLOWED_HOST in setting to * to allow all hosts for now

--- Medicine_Donation_System_for_NGOs/settings.py
+++ Medicine_Donation_System_for_NGOs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-py&)x3_w@v$b(7(z8y+t%kkob#oh4$nvp91i=@2raokn(mx!r=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
